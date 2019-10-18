3fc4297f9bc01d3adcd2ff477b7e16f36a392e6b
yiiitsahin <ygtshn@gmail.com>
2019-10-19 01:47:22 +0300

    First init

---
+++ README.md
@@ -0,0 +1,14 @@
+## 🚀 Webpack 4 Boilerplate
+
+A boilerplate for my personal projects. There are separate config files for development and production. If you like it you can use or edit or whatever you want.
+
+For development:
+```
+npm run start
+```
+
+For production:
+
+```
+npm run build
+```
---
+++ webpack.config.prod.js
@@ -0,0 +1,66 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+config = {
+	mode: 'production',
+	entry: './src/index.js',
+	output: {
+		filename: '[name].[chunkhash].js',
+		path: path.resolve(__dirname, 'dist')
+	},
+	plugins: [
+		new webpack.ProgressPlugin(),
+		new HtmlWebpackPlugin({
+			template: './public/index.html',
+		}),
+		new MiniCssExtractPlugin({
+			filename: '[name].[hash].css',
+			chunkFilename: '[id].[hash].css'
+		}),
+		new CleanWebpackPlugin()
+	],
+	module: {
+		rules: [
+			{
+				test: /.(js|jsx)$/,
+				include: [path.resolve(__dirname, 'src')],
+				loader: 'babel-loader',
+				options: {
+					plugins: ['syntax-dynamic-import'],
+					presets: [
+						[
+							'@babel/preset-env',
+							{
+								modules: false
+							}
+						]
+					]
+				}
+			},
+			{
+				test: /\.scss$/,
+				use: [
+					// style-loader
+					{ loader: MiniCssExtractPlugin.loader },
+					// css-loader
+					{
+						loader: 'css-loader',
+						options: {
+							modules: true
+						}
+					},
+					// sass-loader
+					{ loader: 'sass-loader' }
+				]
+			}
+		]
+	},
+	devServer: {
+		open: false
+	}
+};
+
+module.exports = config;
